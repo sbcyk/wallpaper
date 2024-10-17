7d9b7bda04d8afac989816f859e6c64ee828f5cb
cyk <sdlxcyk@outlook.com>
2024-10-17 12:02:00 +0800

    自定义头部导航栏

---
+++ components/custom-nav-bar/custom-nav-bar.vue
@@ -0,0 +1,81 @@
+<template>
+	<view class="layout">
+		<view class="navBar">
+			<view class="stateBar"></view>
+			
+			<view class="titleBar">
+				<view class="title">标题</view>
+				<view class="search">
+					<uni-icons class="icon" type="search" color="#888" size="18"></uni-icons>
+					<text class="text">搜索</text>
+				</view>
+			</view>
+		</view>
+		
+		<view class="fill">
+			
+		</view>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.layout {
+	.navBar {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		z-index: 10;
+		background:
+		 linear-gradient(to bottom, transparent, #fff 400rpx),
+		 linear-gradient(to right, #beecd8 20%, #F4E2D8);
+			
+		.stateBar {
+			// border: 1px solid red;
+		}
+		
+		.titleBar {
+			display: flex;
+			align-items: center;
+			padding: 0 30rpx;
+			// border: 1px solid green;
+			
+			.title {
+				font-size: 22px;
+				font-weight: 700;
+				color: $text-font-color-1;
+			}
+			
+			.search {
+				width: 220rpx;
+				height: 50rpx;
+				border-radius: 60rpx;
+				background: rgba(255, 255, 255, 0.4);
+				border: 1px solid #fff;
+				margin-left: 30rpx;
+				color: #999;
+				font-size: 28rpx;
+				display: flex;
+				align-items: center;
+				
+				.text {
+					padding-left: 10rpx;
+					
+				}
+				
+				.icon {
+					margin-left: 5rpx;
+				}
+			}
+		}
+	}
+	
+	.fill {
+		
+	}
+}
+</style>
