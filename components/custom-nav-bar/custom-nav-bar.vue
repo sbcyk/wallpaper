7b302e85cc9cd0ca5e0e34fcfb766c6f83d77cf9
cyk <sdlxcyk@outlook.com>
2024-10-17 14:14:28 +0800

    自定义头部内容完成

--- components/custom-nav-bar/custom-nav-bar.vue
+++ components/custom-nav-bar/custom-nav-bar.vue
@@ -1,9 +1,9 @@
 <template>
 	<view class="layout">
 		<view class="navBar">
-			<view class="stateBar"></view>
+			<view class="stateBar" :style="{height: statusBarHeight + 'px'}"></view>
 			
-			<view class="titleBar">
+			<view class="titleBar" :style="{height: titleBarHeight + 'px'}">
 				<view class="title">标题</view>
 				<view class="search">
 					<uni-icons class="icon" type="search" color="#888" size="18"></uni-icons>
@@ -12,13 +12,20 @@
 			</view>
 		</view>
 		
-		<view class="fill">
-			
+		<view class="fill" :style="{height: statusBarHeight + titleBarHeight + 'px'}">
+			<!-- 填充区域将状态栏和标题栏的位置填充，避免绝对定位的标题栏盖住下方内容 -->
 		</view>
 	</view>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+let SYSTEM_INFO = uni.getSystemInfoSync();
+let statusBarHeight = ref(SYSTEM_INFO.statusBarHeight)
+
+let {top, height} = uni.getMenuButtonBoundingClientRect();
+// console.log({top, height})
+let titleBarHeight = ref((top - statusBarHeight.value) * 2 + height)
 
 </script>
 
