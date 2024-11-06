338a46b1968498370faa35c0d4677eeaa5b9b14d
cyk <sdlxcyk@outlook.com>
2024-11-06 16:20:08 +0800

    部分内容使用外部接口

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,12 @@
 <template>
 	<view class="homeLayout pageBg">
 		<custom-nav-bar title="推荐"></custom-nav-bar>
-		
+
 		<view class="banner">
-			<swiper indicator-dots indicator-color="rgba(255,255,255,0.5)" indicator-active-color="#fff" autoplay circular>
-				<swiper-item v-for="item in 3">
-					<image src="../../common/images/banner2.jpg" mode="aspectFill"></image>
+			<swiper indicator-dots indicator-color="rgba(255,255,255,0.5)" indicator-active-color="#fff" autoplay
+				circular>
+				<swiper-item v-for="item in bannerList" :key="item._id">
+					<image :src="item.picurl" mode="aspectFill"></image>
 				</swiper-item>
 			</swiper>
 		</view>
@@ -17,8 +18,8 @@
 			</view>
 			<view class="center">
 				<swiper vertical autoplay interval="1500" duration="300" circular>
-					<swiper-item v-for="item in 4">
-						<navigator url="/pages/notice/detail">文字内文字文字文字文字文字文字文字文字容</navigator>
+					<swiper-item v-for="item in notice" :key="item._id">
+						<navigator url="/pages/notice/detail">{{ item.title }}</navigator>
 					</swiper-item>
 				</swiper>
 			</view>
@@ -26,28 +27,28 @@
 				<uni-icons type="right" size="16" color="#333"></uni-icons>
 			</view>
 		</view>
-		
+
 		<view class="select">
 			<common-title>
 				<template #name>每日推荐</template>
 				<template #custom>
 					<view class="date">
-						<uni-icons type="calendar" size="18"></uni-icons>	
+						<uni-icons type="calendar" size="18"></uni-icons>
 						<view class="text">
-							<uni-dateformat :date="Date.now()" format="dd日"></uni-dateformat>	
+							<uni-dateformat :date="Date.now()" format="dd日"></uni-dateformat>
 						</view>
 					</view>
 				</template>
 			</common-title>
 			<view class="content">
 				<scroll-view scroll-x>
-					<view class="box" v-for="item in 8" @click="goPreview()">
-						<image src="../../common/images/preview_small.webp" mode="aspectFill"></image>
+					<view class="box" v-for="item in radomList" :key="item._id" @click="goPreview()">
+						<image :src="item.smallPicurl" mode="aspectFill"></image>
 					</view>
 				</scroll-view>
 			</view>
 		</view>
-		
+
 		<view class="theme">
 			<common-title>
 				<template #name>专题精选</template>
@@ -55,7 +56,7 @@
 					<navigator url="" class="more">more</navigator>
 				</template>
 			</common-title>
-			
+
 			<view class="content">
 				<theme-item v-for="item in 8"></theme-item>
 				<theme-item :isMore="true"></theme-item>
@@ -65,11 +66,67 @@
 </template>
 
 <script setup>
-const goPreview = () => {
-	uni.navigateTo({
-		url: "/pages/preview/preview"
-	})
-}
+	import {
+		ref
+	} from 'vue';
+
+	const bannerList = ref([])
+	const radomList = ref([])
+	const notice = ref([])
+
+
+	const goPreview = () => {
+		uni.navigateTo({
+			url: "/pages/preview/preview"
+		})
+	}
+
+	const getBanner = async () => {
+		let res = await uni.request({
+			url: "https://tea.qingnian8.com/api/bizhi/homeBanner",
+			header: {
+				"access-key": "cykkey"
+			}
+		})
+		// console.log(res)
+		if (res.data.errCode === 0) {
+			bannerList.value = res.data.data
+		}
+	}
+
+	const getDayRadom = async () => {
+		let res = await uni.request({
+			url: "https://tea.qingnian8.com/api/bizhi/randomWall",
+			header: {
+				"access-key": "cykkey"
+			}
+		})
+		// console.log(res)
+		if (res.data.errCode === 0) {
+			radomList.value = res.data.data
+		}
+	}
+
+	const getNotice = async () => {
+		let res = await uni.request({
+			url: "https://tea.qingnian8.com/api/bizhi/wallNewsList",
+			header: {
+				"access-key": "cykkey"
+			},
+			data: {
+				select: true
+				// pageSize: 3
+			}
+		})
+		console.log(res)
+		if (res.data.errCode === 0) {
+			notice.value = res.data.data
+		}
+	}
+
+	getBanner();
+	getDayRadom();
+	getNotice();
 </script>
 
 <style lang="scss" scoped>
@@ -96,7 +153,7 @@ const goPreview = () => {
 			}
 
 		}
-		
+
 		.notice {
 			width: 690rpx;
 			height: 80rpx;
@@ -105,31 +162,33 @@ const goPreview = () => {
 			margin: 0 auto;
 			border-radius: 80rpx;
 			display: flex;
-			
+
 			.left {
 				width: 140rpx;
 				display: flex;
 				align-items: center;
 				justify-content: center;
+
 				:deep() {
 					.uni-icons {
 						color: $brand-theme-color !important;
 					}
 				}
 
-				
+
 				.text {
 					color: $brand-theme-color;
 					font-weight: 600;
 					font-size: 28rpx;
 				}
 			}
+
 			.center {
 				flex: 1;
-				
+
 				swiper {
 					height: 100%;
-					
+
 					&-item {
 						height: 100%;
 						font-size: 30rpx;
@@ -140,6 +199,7 @@ const goPreview = () => {
 					}
 				}
 			}
+
 			.right {
 				width: 70rpx;
 				display: flex;
@@ -147,61 +207,62 @@ const goPreview = () => {
 				justify-content: center;
 			}
 		}
-		
+
 		.select {
 			padding-top: 50rpx;
-			
+
 			.content {
 				width: 720rpx;
 				margin-left: 30rpx;
 				margin-top: 30rpx;
-				
+
 				scroll-view {
 					white-space: nowrap;
-					
+
 					.box {
 						width: 200rpx;
 						height: 430rpx;
 						display: inline-block;
 						margin-right: 15rpx;
-						
+
 						image {
 							width: 100%;
 							height: 100%;
 							border-radius: 10rpx;
 						}
 					}
-					
+
 					.box:last-child {
 						margin-right: 30rpx;
 					}
 				}
 			}
-			
+
 			.date {
 				color: $brand-theme-color;
 				display: flex;
 				align-items: center;
+
 				:deep() {
 					.uni-icons {
 						color: $brand-theme-color !important;
 					}
 				}
-				
+
 				.text {
 					margin-left: 5rpx;
 				}
 			}
 		}
-		
+
 		.theme {
 			padding: 50rpx 0;
-			
+
 			.more {
 				font-size: 32rpx;
 				color: $text-font-color-3;
 			}
-			
+
 			.content {
 				margin-top: 30rpx;
 				padding: 0 30rpx;
